3946b946a1c1f8058fb971dccd33419a81f010fc
tommaclean <tom.j.maclean@gmail.com>
2021-06-07 15:37:41 -0400

    fist commit\!

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Auth0 from 'react-native-auth0';
+const auth0 = new Auth0({ domain: 'klatchevents.us.auth0.com', clientId: 'BkbzakXDcbYnltwoM4tyKHGrm1z6YM9M' });
 
 export default function App() {
+  const [accessToken, setAccessToken] = useState(null)
+  const logIn = () => {
+    auth0
+    .webAuth
+    .authorize({scope: 'openid profile email'})
+    .then(credentials =>
+      // Successfully authenticated
+      // Store the accessToken
+      setAccessToken(credentials)
+    )
+    .catch(error => console.log(error));
+  }
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Klatch</Text>
+      <Button onPress={logIn} title={"Log In"}/>
       <StatusBar style="auto" />
     </View>
   );
